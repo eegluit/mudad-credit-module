d5cc15c840fec8e8cb39d61a034b5d8774816abe
Sankalp <sankalp.jain3998@gmail.com>
2023-01-14 18:16:47 +0530

    Updated the Personal Information Forms

--- pages/1_forms.py
+++ pages/1_forms.py
@@ -4,21 +4,35 @@ import re
 st.set_page_config(page_title="User Form", page_icon=":guardsman:", layout="wide")
 
 def show_form():
-
+    st.title("Personal Information")
     with st.form("unique_form_key"):
-        name = st.text_input("Enter your name:")
+        f_name = st.text_input("Enter your First name:")
+        l_name = st.text_input("Enter your Last name:")
+        gender = st.selectbox("Select your gender:", ["Male", "Female", "Other"])
         email = st.text_input("Enter your email:")
         age = st.text_input("Enter your age:")
-        gender = st.selectbox("Select your gender:", ["Male", "Female", "Other"])
+        profession = st.text_input("Enter your Profession: ")
+        employer = st.text_input("Enter your Employer Name")
+        employer_add = st.text_input("Enter your Employer's Address: ")
+        base_salary = st.text_input("Enter your Monthly Base Salary: ")
+        
+        
         # attachment = st.file_uploader("Upload your attachment:", type=["csv", "txt", "pdf"])
         if st.form_submit_button('Submit'):
             email_pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
             if email_pattern.match(email):
                 if age.isnumeric():
-                    st.success("Name: " + name)
+                    st.success("First Name: " + f_name)
+                    st.success("Last Name: " + l_name)
                     st.success("Email: " + email)
                     st.success("Age: " + age)
                     st.success("Gender: " + gender)
+                    st.success("Profession: " + profession)
+                    st.success("Employer Name: " + employer)
+                    st.success("Employer Address: " + employer_add)
+                    st.success("Your Monthly Base Salary: " + base_salary)
+
+
                     
                     st.write("Thank you for submitting the form.")
                     st.markdown("<style> .stFormContainer{display: none;} </style>", unsafe_allow_html=True)
